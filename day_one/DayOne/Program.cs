d454a13979a4a3d74d8d283208a57177b8602029
tkshillinz <tkshillinz@icloud.com>
2020-12-01 23:52:37 -0400

    day one complete

--- day_one/DayOne/Program.cs
+++ day_one/DayOne/Program.cs
@@ -1,14 +1,88 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace DayOne
 {
+    enum Part
+    {
+        One,
+        Two
+    }
     class Program
     {
+        static IEnumerable<int> TryFindPair(IEnumerable<int> input)
+        {
+            foreach (var (item, index) in input.Select((value, i) => (value, i)))
+            {
+                IEnumerable<int> remaining = input.Skip(index + 1);
+                foreach (int item2 in remaining)
+                {
+                    int[] values = { item, item2 };
+                    if (AreIntsEqualTo(2020, values))
+                    {
+                        return values;
+                    }
+                }
+            }
+            return Enumerable.Empty<int>();
+        }
+
+        static IEnumerable<int> TryFindTriplet(IEnumerable<int> input)
+        {
+            foreach (var (item, index) in input.Select((value, i) => (value, i)))
+            {
+                IEnumerable<int> afterValueOne = input.Skip(index + 1);
+
+                foreach (var (item2, index2) in afterValueOne.Select((value, i) => (value, i)))
+                {
+                    IEnumerable<int> afterValueTwo = input.Skip(index2 + 1);
+
+                    foreach (int item3 in afterValueTwo)
+                    {
+                        int[] values = { item, item2, item3 };
+                        if (AreIntsEqualTo(2020, values))
+                        {
+                            return values;
+                        }
+                    }
+                }
+            }
+            return Enumerable.Empty<int>();
+        }
+
+        static IEnumerable<int> FilePathToList(string filepath)
+        {
+            return File.ReadAllLines(filepath)
+                .Select(int.Parse);
+        }
+
+        static bool AreIntsEqualTo(int check, IEnumerable<int> values)
+        {
+            return (Enumerable.Sum(values) == check);
+        }
+
+        static IEnumerable<int> TryFindResults(Part part, IEnumerable<int> input)
+        {
+            if (part == Part.One)
+            {
+                return TryFindPair(input);
+            }
+            return TryFindTriplet(input);
+        }
+
         static void Main(string[] args)
         {
+            const String inputFilePath = "./input.txt";
 
+            IEnumerable<int> inputList = FilePathToList(inputFilePath);
 
-            Console.WriteLine("Hello World!");
+            IEnumerable<int> result = TryFindResults(Part.Two, inputList);
+
+            int answer = result.Aggregate(1, (prod, next) => prod * next);
+
+            Console.WriteLine(answer);
         }
     }
 }
