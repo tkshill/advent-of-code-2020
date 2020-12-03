eb7d66e7f628464c1e211f6ea37c50fa8a3cdccc
tkshillinz <tkshillinz@icloud.com>
2020-12-02 22:43:00 -0400

    refactored linq logic

--- AOC2020/DayTwo/DayTwo.cs
+++ AOC2020/DayTwo/DayTwo.cs
@@ -4,6 +4,22 @@ using System.Linq;
 
 namespace DayTwo
 {
+    public readonly struct Token
+    {
+        public Token(int x, int y, char key, string password)
+        {
+            First = x;
+            Second = y;
+            Key = key;
+            Password = password;
+        }
+        public int First { get; }
+
+        public int Second { get; }
+
+        public char Key { get; }
+        public string Password { get; }
+    }
     public class Result : IResult
     {
         public string filepath() => "./DayTwo/input.txt";
@@ -11,12 +27,14 @@ namespace DayTwo
         public string PartOne(IEnumerable<string> input) =>
             input
             .Select(SplitLine)
+            .Select(ToToken)
             .Count(x => IsValidPassWord1(x))
             .ToString();
 
         public string PartTwo(IEnumerable<string> input) =>
             input
             .Select(SplitLine)
+            .Select(ToToken)
             .Count(x => IsValidPassWord2(x))
             .ToString();
 
@@ -29,30 +47,29 @@ namespace DayTwo
             return (arr[0], arr[1], arr[2]);
         }
 
-        static bool IsValidPassWord1((string, string, string) token)
+        static Token ToToken((string, string, string) input)
         {
-            IEnumerable<int> rangeSplit = token.Item1.Split("-").Select(x => int.Parse(x));
 
-            (int startRange, int endRange) = (rangeSplit.First(), rangeSplit.Last());
-
-            char key = token.Item2.First();
-            int count = token.Item3.Count(x => x == key);
-
-            return (count >= startRange && count <= endRange);
-
-        }
-
-        static bool IsValidPassWord2((string, string, string) token)
-        {
-            IEnumerable<int> rangeSplit = token.Item1.Split("-").Select(x => int.Parse(x));
-
+            IEnumerable<int> rangeSplit = input.Item1.Split("-").Select(x => int.Parse(x));
             (int pos1, int pos2) = (rangeSplit.First(), rangeSplit.Last());
+            char key = input.Item2.First();
+            string password = input.Item3;
 
-            char key = token.Item2.First();
-            string password = token.Item3;
-
-            return (key == password[pos1 - 1] ^ key == password[pos2 - 1]);
+            return new Token(pos1, pos2, key, password);
         }
-}
+
+        static bool IsValidPassWord1(Token token)
+        {
+            int count = token.Password.Count(x => x == token.Key);
+
+            return (count >= token.First && count <= token.Second);
+        }
+
+        static bool IsValidPassWord2(Token token) =>
+            (
+                token.Key == token.Password[token.First - 1] ^
+                token.Key == token.Password[token.Second - 1]
+            );
+    }
 }
 
